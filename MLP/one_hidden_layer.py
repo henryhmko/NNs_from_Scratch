5945c864b4c1ea5f321623cb94a6d15c1fe648c8
Henry HM Ko <hmkoyeaa@gmail.com>
2024-04-26 04:39:27 +0000

    Added more explanations to one layer MLP numpy version

--- MLP/one_hidden_layer.py
+++ MLP/one_hidden_layer.py
@@ -8,7 +8,7 @@ class MLP:
         self.num_iterations = num_iterations
         # Inititalize hidden weights as a matrix of (input_size x hidden_size)
         # NOTE: initializing weights with .randn leads to failed training more than ~50% of the time for this code
-        # TODO: Must initialize weights with NON-NEGATIVE rational numbers
+        # NOTE: Must initialize weights with NON-NEGATIVE rational numbers
         self.weights_hidden = np.random.rand(input_size, hidden_size) # Recall that .randn outputs values between [-1, 1] vs .rand outputs [0, 1)
         # Initialize hidden bias as a vector of zeros with number of elements equal to hidden_size
         self.bias_hidden = np.zeros(hidden_size)
@@ -21,7 +21,7 @@ class MLP:
         return np.maximum(0, x)
 
     def _relu_derivative(self, x):
-        return (x > 0).astype(float) # In ML, we pretend relu is differentiable by saying the derivative at x=0 is 0
+        return (x > 0).astype(float) # In ML, we "pretend" relu is differentiable by defining the derivative at x=0 is 0
     
     def _forward(self, X):
         self.hidden_layer = self._relu(X @ self.weights_hidden + self.bias_hidden)
@@ -29,10 +29,13 @@ class MLP:
         return self.output_layer
 
     def _backward(self, x, y, output):
-        error_output = output - y.reshape(-1, 1) # Practice more reshaping - I should know which dimensions these are
-        # Compute gradient of the loss w.r.t the output layer
-        # calculated by multiplying the error at the output layer with the derivative of the ReLU activation function
-        gradient_output = error_output * self._relu_derivative(output)
+        # Recall that our loss is the MSE loss
+        # error_output is the gradient of the loss w.r.t the outputs of the network
+        # NOTE: technically, error_output should be `error_output = (2/n) * (y_pred - y_true)` but the scaling term of (2/n) is dropped since we are mainly interested in the direction fo the gradient
+        error_output = output - y.reshape(-1, 1) #which reshapes y into the form of output
+        # Apply the chain rule 
+        gradient_output = error_output * self._relu_derivative(output) # Recall that output == self.output_layer
+
         error_hidden = gradient_output @ self.weights_output.T
         gradient_hidden = error_hidden * self._relu_derivative(self.hidden_layer)
         
@@ -41,7 +44,7 @@ class MLP:
         self.bias_output -= self.learning_rate * np.sum(gradient_output, axis=0) 
         self.weights_hidden -= self.learning_rate * (x.T @ gradient_hidden)
         self.bias_hidden -= self.learning_rate * np.sum(gradient_hidden, axis=0)
-    
+
     def fit(self, X, y):
         for _ in range(self.num_iterations):
             output = self._forward(X)
